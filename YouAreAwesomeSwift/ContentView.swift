066d59f098f6b26ead484d21fa60c50e36c8ca63
Doughy <Doughyvibe@gmail.com>
2025-11-07 18:27:57 +0800

    Chapter 1.3 Done!

--- YouAreAwesomeSwift/ContentView.swift
+++ YouAreAwesomeSwift/ContentView.swift
@@ -11,21 +11,33 @@ struct ContentView: View {
     @State private var message = "I am a Programmer!!"
     var body: some View {
         VStack {
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text(message)
-                .foregroundStyle(.red)
-                .font(.largeTitle)
-                .fontWeight(.bold)
-            Button("Click me!") {
-                message = "Awesome!!"
-            }
-            Button("Click me too") {
-                message = "Super!!"
-                }
+                .frame(width: 200, height: 200)
             
+            Text(message)
+                .foregroundStyle(.black)
+                .fontWeight(.ultraLight)
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            HStack {
+                Button("Click me!") {
+                    message = "Awesome!!"
+                }
+                
+                Button("Click me too") {
+                    message = "Super!!"
+                }
+            }
+            .buttonStyle(.glassProminent)
+            .font(.largeTitle)
+            .tint(.purple)
         }
         .padding()
     }
