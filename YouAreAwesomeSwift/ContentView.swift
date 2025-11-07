a6d30587eb0b4f7a74eb902a24627f4cbe75d931
Doughy <Doughyvibe@gmail.com>
2025-11-07 17:04:27 +0800

    Initial Commit

---
+++ YouAreAwesomeSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesomeSwift
+//
+//  Created by Doughy on 7/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesomeSwift/YouAreAwesomeSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeSwiftApp.swift
+//  YouAreAwesomeSwift
+//
+//  Created by Doughy on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
