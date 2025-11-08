45998afdc84a3d13ddbe923063fe903b1de13e50
Doughy <Doughyvibe@gmail.com>
2025-11-08 16:49:21 +0800

    State & Ternary Op

--- YouAreAwesomeSwift/ContentView.swift
+++ YouAreAwesomeSwift/ContentView.swift
@@ -22,22 +22,36 @@ struct ContentView: View {
             
             Text(message)
                 .foregroundStyle(.black)
-                .fontWeight(.heavy)
+                .fontWeight(.thin)
                 .font(.largeTitle)
             
             Spacer()
             
             Button("Press me!") {
+                
                 let message1 = "You are awesome!"
                 let message2 = "You are Great!"
                 let img1 = "sun.max.fill"
                 let img2 = "hand.thumbsup"
+//                if message == message1 {
+//                    imageView = img1
+//                    message = message2
+//                } else {
+//                    imageView = img2
+//                    message = message1
+//                 }
+                //Ternary operator
+//                message = (message == message1 ? message2 : message1)
+//                imageView = (imageView == img1 ? img2 : img1)
                 
-                if message == message1 {
-                    imageView = img1
-                    message = message2
-                } else {
+                //Switch case++
+                
+                switch message {
+                case message1:
                     imageView = img2
+                    message = message2
+                default:
+                    imageView = img1
                     message = message1
                 }
                 
